c9b997c88cbddb4d373ddfc86319773ddd30e1e4
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-16 12:01:02 -0700

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216182441) do
+ActiveRecord::Schema.define(version: 20171216190001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20171216182441) do
     t.text "idea"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
   end
 
 end
