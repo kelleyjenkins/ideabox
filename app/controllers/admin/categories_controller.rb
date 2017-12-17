54e9cdfd597a55682e8abbb220d419ccc1d5ab9c
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-17 16:01:45 -0700

    admin adds new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,43 @@
 class Admin::CategoriesController < ApplicationController
-
   def index
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    redirect_to admin_categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    if @category.save
+      flash[:success] = "#{@category.name} updated!"
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    category.destroy
+
+    flash[:success] = "#{@category.name} was successfully deleted!"
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
