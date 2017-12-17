e3ba4152abe13b714cf7798fb89868bf2acc3b36
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-17 10:36:24 -0700

    add edit functionality to ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,13 +1,41 @@
 class IdeasController < ApplicationController
   def index
-    @ideas = Idea.all
+    @user = User.find(params[:user_id])
+    @ideas = @user.ideas
   end
 
   def new
-    @idea = Idea.new
+    @user = User.find(params[:user_id])
+    @idea = @user.ideas.new
   end
 
   def create
-    @idea = Idea.create(:idea)
+    user = User.find(params[:user_id])
+    idea = user.ideas.create(idea_params)
+    redirect_to user_path
+  end
+
+  def show
+    @user= User.find(params[:user_id])
+    @idea = Idea.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+    idea = Idea.update(params[:id], idea_params)
+
+    redirect_to user_ideas_path(idea.user, idea)
+  end
+
+
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:idea)
   end
 end
---
+++ spec/features/ideas/user_can_edit_an_idea_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "User can edit an existing idea" do
+  scenario "a user can edit their own ideas" do
+  user = create(:user)
+  idea = create(:idea)
+
+  visit edit_user_idea_path(user, idea)
+
+  fill_in "idea[idea]", with: "New Idea"
+  click_button "Update"
+
+  expect(page).to have_content("New Idea")
+  end
+end
