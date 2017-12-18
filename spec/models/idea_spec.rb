a726de4f1f6850a8ad20dde83cfdf8e193732f9d
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-18 15:37:17 -0700

    factorybot edits and model tests started

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,12 @@
+# require "rails_helper"
+#
+# describe Idea, type: :model do
+#   describe "validations" do
+#     it {should validate_presence_of(:title, :idea)}
+#   end
+#
+#   describe "relationships" do
+#     it {should have_many(:images).through(:idea_images)}
+#
+#   end
+# end
