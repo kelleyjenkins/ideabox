a74bda417c189cfbdfc7442d65030bfbf3120554
Kelley Jenkins <ckjenkins3@gmail.com>
2017-12-20 20:21:36 -0700

    adding styling

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -46,7 +46,7 @@ class IdeasController < ApplicationController
   private
 
   def idea_params
-    params.require(:idea).permit(:title, :idea, :category_id, :image_ids)
+    params.require(:idea).permit(:title, :idea, :category_id, image_ids:[])
   end
 
   def set_idea
